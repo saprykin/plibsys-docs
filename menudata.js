b67570442409052575933fc8fbecf8c6fd52d8fe
Alexander Saprykin <xelfium@gmail.com>
2017-05-08 23:18:09 +0200

    Add version 0.0.3

---
+++ menudata.js
@@ -0,0 +1,25 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Variables",url:"functions_vars.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"f",url:"globals.html#index_f"},
+{text:"p",url:"globals_p.html#index_p"},
+{text:"t",url:"globals_t.html#index_t"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"p",url:"globals_func.html#index_p"}]},
+{text:"Typedefs",url:"globals_type.html",children:[
+{text:"p",url:"globals_type.html#index_p"}]},
+{text:"Enumerations",url:"globals_enum.html"},
+{text:"Enumerator",url:"globals_eval.html",children:[
+{text:"p",url:"globals_eval.html#index_p"}]},
+{text:"Macros",url:"globals_defs.html",children:[
+{text:"f",url:"globals_defs.html#index_f"},
+{text:"p",url:"globals_defs.html#index_p"},
+{text:"t",url:"globals_defs.html#index_t"}]}]}]}]}
